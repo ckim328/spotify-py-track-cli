f28e850faabb27922a507fd0c562835fd73dc526
ckim328 <35876108+ckim328@users.noreply.github.com>
2020-04-14 16:02:55 -0400

    Update authorization.py

--- authorization.py
+++ authorization.py
@@ -4,8 +4,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.oauth2 as oauth2
 
 
-CLIENT_ID = "80499bd993c1435fb442a56bfbe9f939"
-CLIENT_SECRET = "5fed3fe23e7a42b6ac16451425dac965"
+CLIENT_ID = ""
+CLIENT_SECRET = ""
 
 
 class SpotifyCredentials():
